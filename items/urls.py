83726aa855a84de5a5ee5a502ffa89eacce5f94a
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-09 13:43:38 +0000

    Added urls, templates and forms to items app

---
+++ items/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.create_item_view, name='create_item'),
+    path('', views.item_list_view, name='item_list'),
+    path('<int:item_id>/recovered/', views.mark_as_recovered_view, name='mark_as_recovered'),
+    path('<int:item_id>/comment/', views.add_comment_view, name='add_comment'),
+]

b375e63ab4273e754ab9508c3140d598594dcab7
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-09 13:35:53 +0000

    Set debug to True

--- campus_lost_and_found/settings.py
+++ campus_lost_and_found/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-matthieuarn-hackathon2g-oqr7eyixn88.ws.codeinstitute-ide.net', '8000-matthieuarn-hackathon2g-vrvihbhitak.ws.codeinstitute-ide.net','.herokuapp.com']
 

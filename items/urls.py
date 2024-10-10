4f1708ed0cf2c74cd44c1e187a4d61b92b335680
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-10 12:04:58 +0000

    Try things

--- items/urls.py
+++ items/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.indexpage, name='home'),
+    path('', indexpage, name='index'),
     path('create/', views.create_item_view, name='create_item'),
     path('', views.item_list_view, name='item_list'),
     path('<int:item_id>/recovered/', views.mark_as_recovered_view, name='mark_as_recovered'),

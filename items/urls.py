6fc1e187d8a450b93c882f776e3e0b76af5a1946
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-10-10 12:01:43 +0000

    Changed items list url path

--- items/urls.py
+++ items/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.indexpage, name='home'),
     path('create/', views.create_item_view, name='create_item'),
-    path('', views.item_list_view, name='item_list'),
+    path('items/', views.item_list_view, name='item_list'),
     path('<int:item_id>/recovered/', views.mark_as_recovered_view, name='mark_as_recovered'),
     path('<int:item_id>/comment/', views.add_comment_view, name='add_comment'),
 ]

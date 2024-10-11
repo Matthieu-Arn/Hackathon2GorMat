e3d8c3d2a6005b43475016724fd55e5fe6d07068
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-11 09:06:10 +0000

    Fix summernote package issues

--- campus_lost_and_found/urls.py
+++ campus_lost_and_found/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     # path("", include("items.urls"), name="items-urls"),
     path('admin/', admin.site.urls),
     path('items/', include('items.urls')),
-    path('summernote/', include('summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
